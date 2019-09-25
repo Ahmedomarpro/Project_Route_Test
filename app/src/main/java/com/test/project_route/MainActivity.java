398935d6f813d2facc0bc9c485581c99d7d9fecd
Osama Ibrahim <oimigy@live.com>
2019-09-25 13:42:47 +0200

    done with MainActivity, created AndroidActivity, IosActivity and FullStackActivity

--- app/src/main/java/com/test/project_route/MainActivity.java
+++ app/src/main/java/com/test/project_route/MainActivity.java
@@ -2,7 +2,9 @@ package com.test.project_route;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewDetails(View view) {
+        int id = view.getId();
+        Intent intent = null;
+
+        switch (id) {
+            case R.id.btn_android:
+                intent = new Intent(this, AndroidActivity.class);
+                break;
+            case R.id.btn_ios:
+                intent = new Intent(this, IosActivity.class);
+                break;
+            case R.id.btn_fullstack:
+                intent = new Intent(this, FullStackActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
